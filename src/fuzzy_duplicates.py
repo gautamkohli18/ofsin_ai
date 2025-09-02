666c0b0eb3929a2c1c83ae6cf3f927f5198af9da
Gautam Thulasiraman <111104742+gautamkohli18@users.noreply.github.com>
2025-09-02 15:19:34 +0530

    Update fuzzy_duplicates.py

--- src/fuzzy_duplicates.py
+++ src/fuzzy_duplicates.py
@@ -2,13 +2,28 @@ from fuzzywuzzy import fuzz
 
 def detect_potential_duplicates(transactions_df):
     duplicates = []
+    cols = transactions_df.columns
+
+    # Choose which field to compare
+    if "description" in cols:
+        field = "description"
+    elif "merchant" in cols:
+        field = "merchant"
+    elif "details" in cols:
+        field = "details"
+    else:
+        return []  # no comparable field
+
     for i in range(len(transactions_df)):
         for j in range(i + 1, len(transactions_df)):
-            desc1 = str(transactions_df.iloc[i]["description"])
-            desc2 = str(transactions_df.iloc[j]["description"])
-            if fuzz.ratio(desc1, desc2) > 90:
+            val1 = str(transactions_df.iloc[i][field])
+            val2 = str(transactions_df.iloc[j][field])
+            if fuzz.ratio(val1, val2) > 90:
                 duplicates.append((
                     transactions_df.iloc[i].get("transaction_id", i),
-                    transactions_df.iloc[j].get("transaction_id", j)
+                    transactions_df.iloc[j].get("transaction_id", j),
+                    field,
+                    val1,
+                    val2
                 ))
     return duplicates
